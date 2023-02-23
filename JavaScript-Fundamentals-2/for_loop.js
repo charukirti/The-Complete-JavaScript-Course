0a21e3f96b468b0307476b18b3fffb2c4e5f1229
charukirti <charukirticc112002@gmail.com>
2023-02-24 00:20:29 +0530

    continued with for loop

--- JavaScript-Fundamentals-2/for_loop.js
+++ JavaScript-Fundamentals-2/for_loop.js
@@ -5,22 +5,22 @@
 }*/
 
 
-// for(let i = 1; i <= 10; i++) {
-//     console.log(`${"i * i"} = ${i * i}`)
-// }
+for(let i = 1; i <= 10; i++) {
+    console.log(`${"i * i"} = ${i * i}`)
+}
 
-// for(let n = 1; n <= 10; n++) {
-//     for(let m = 1; m <= 10; m++) {
-//         console.log(`${n * m}`)
-//     }
-// }
+for(let n = 1; n <= 10; n++) {
+    for(let m = 1; m <= 10; m++) {
+        console.log(`${n * m}`)
+    }
+}
 
 
 const solarSys = ['Mars','Earth', 'Mercury', 'Saturn', 'Venus', 'Jupitor', 'Uranus'];
 
-// for(let i = 0; i < solarSys.length; i++) {
-//     console.log(solarSys[i])
-// }
+for(let i = 0; i < solarSys.length; i++) {
+    console.log(solarSys[i])
+}
 
 const birthYears = [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009];
 
