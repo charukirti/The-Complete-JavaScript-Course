cd88a827831a47eabf2e1404812659edd9b3050e
charukirti <charukirticc112002@gmail.com>
2023-03-01 23:01:45 +0530

    Completed Coding Challenge

---
+++ Developer Skills/challenge-1.js
@@ -0,0 +1,11 @@
+const tempretures = [17, 21, 23];
+
+const printForecast = function (arr) {
+    let str = '';
+    for(let i = 0; i < arr.length; i++) {
+        str = str + `${arr[i]}°C in ${i + 1} days... `;
+    }
+    console.log(' ...' + str);
+}
+
+console.log(printForecast(tempretures))
