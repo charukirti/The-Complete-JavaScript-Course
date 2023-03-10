d921c585f995f514e0f5febbf6f9a442818cf9fe
charukirti <charukirticc112002@gmail.com>
2023-03-10 22:10:07 +0530

    Revised scope chain and lexicle scope

--- How JS works behind the scene/scope-scopeChain.js
+++ How JS works behind the scene/scope-scopeChain.js
@@ -1,21 +1,21 @@
-// * JavaScript Scoping
+/* Scope:- Scope is way to access variables, functions and objects in some perticular part of your 
+code during runtime.
 
-/* There are three types of scope*/
-// 1. Global Scope
-// Its in the outside of function block
-// Variables declared in global scope are acessible everywhere
+* There is a three type of scopes
+1. Global scope :- 
+        - Outside of any function or block
+        - Variables declared in the global scope can be accessed from anywhere
 
-// 2. Function Scope
-// Variables are only accessible inside a function, not outside
-// also called as a local scope
+2. Function Scope :-
+        - Variable can be accessed inside a function block
+        - This also called as local scope
 
-// 3. Block Scope
-// In this variables are only accessible inside block 
-// it only applies for let and const variables
+3. Block Scope :-
+        - This can be accessed inside a block
+        - This can be only applies for let and const
+        - Functions are also block scope in sctrict mode
+*/
 
-/* Variable with  */
-
-// 'use strict'
 
 function calcAge(birthYear) {
     const age = 2037 - birthYear;
@@ -49,3 +49,67 @@ function calcAge(birthYear) {
 const firstName = 'charukirti';
 
 console.log(calcAge(2002))
+
+
+
+
+// Examples 💡
+
+/* 1. Global Scope */
+
+var a = 123; // Globally scoped variable
+function printVar1() {
+  console.log(a);
+}
+
+printVar1(); // 12
+
+/* Local Scope */
+
+function localScope() {
+  const j = 15;
+  console.log("This is locally scoped variable", j);
+}
+
+localScope();
+
+// * Example 2
+
+/* Now if I declare this variable outside of a function block and try to access it. then it will give me a error */
+
+// function localScope() {
+  // console.log('This is locally scoped variable',j);
+// }
+// const j = 15;
+// localScope();
+
+// * Example 3
+
+function printVar2() {
+    var k = 19;
+    console.log(k);
+}
+
+printVar2()
+
+// console.log(k);
+
+/* Block Scope */
+
+var name = 'charukirti';
+function blockScope() {
+    console.log('This is block scope');
+    const age = 20
+    let isElligible = true;
+    if(age >= 18 && isElligible == true) {
+        console.log('You are elligible to become driver');
+    } 
+
+    function nestedBlock() {
+        const currentRole = 'student';
+        console.log(`${name} is a ${age} old, ${currentRole}`);
+    }
+    nestedBlock();
+}
+
+blockScope()
