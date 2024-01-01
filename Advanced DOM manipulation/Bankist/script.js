bacfd03e0b6282621b44de250f0f37aca761e8f3
charukirti <charukirticc112002@gmail.com>
2024-01-01 17:49:51 +0530

    Learnt about DOM life-cycle

--- Advanced DOM manipulation/Bankist/script.js
+++ Advanced DOM manipulation/Bankist/script.js
@@ -394,3 +394,18 @@ slider();
 // console.log(h1.nextSibling);
 
 // console.log(h1.parentElement.children);
+
+document.addEventListener('DOMContentLoaded', function(e){
+  console.log('HTML parsed and dom tree built', e);
+})
+
+
+window.addEventListener('load', function(e){
+  console.log('Page fully loaded', e);
+})
+
+window.addEventListener('beforeunload', function(e){
+  e.preventDefault()
+  console.log(e);
+  e.returnValue = ''
+})
